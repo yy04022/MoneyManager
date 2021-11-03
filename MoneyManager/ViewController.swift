ca46508fcaddf988ca64283c19ea00a75eb5376d
Jinyu Wang <ellie5201314@gmail.com>
2021-11-03 18:42:25 -0400

    final

--- MoneyManager/ViewController.swift
+++ MoneyManager/ViewController.swift
@@ -12,8 +12,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    @IBAction func Login(_ sender: Any) {
+    }
+    
+    @IBAction func Update(_ sender: Any) {
+    }
+    @IBOutlet weak var LinktoSpending: UIButton!
+    
+    @IBOutlet weak var LinktoIncome: UIButton!
 }
 
